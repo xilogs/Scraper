caff47906e5392c27b7753efbb023e50f11cf916
Rizki Engine <tururizki21@gmail.com>
2026-02-07 12:06:23 +0700

    Update doronime.js

--- doronime.js
+++ doronime.js
@@ -6,7 +6,6 @@ class DoronimeScraper {
     constructor() {
         this.baseURL = 'https://doronime.id';
         this.headers = {
-            'authority': 'doronime.id',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
             'priority': 'u=0, i',
